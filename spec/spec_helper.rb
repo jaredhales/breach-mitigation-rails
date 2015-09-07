b9d8630f337a327aa673f71fe782f743ae0b9f1b
Jon Atack <jonnyatack@gmail.com>
2015-09-07 15:47:42 +0200

    Fix rspec-expectations `should` syntax deprecation message
    Using `should` from rspec-expectations' old `:should` syntax without
    explicitly enabling the syntax is deprecated. Use the new `:expect`
    syntax or explicitly enable `:should` with `config.expect_with(:rspec)
    { |c| c.syntax = :should }` instead.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'securerandom'
 require 'breach-mitigation-rails'
 
 RSpec.configure do |config|
+  config.expect_with(:rspec) { |c| c.syntax = :should }
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
